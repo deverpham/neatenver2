fc340893591226609f898eb55d0f18a81caa83ef
deverpham <deverpham@gmail.com>
2016-09-14 03:01:28 +0700

    sdasd
    asdasd

---
+++ index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Appy Page</title>
+    <link rel="stylesheet" href=" ./css/normalize.css">
+    <link rel="stylesheet" href=" ./css/hack.css">
+    <link rel="stylesheet" href=" ./css/apply.css">
+    <link rel="stylesheet" href=" ./css/animate.css">
+  </head>
+  <body froninit="hack">
+    <div class="full-width intro"><img src="images/background.jpg" class="responsive"><a class="button-intro animated infinite bounce"><img src="./images/mouse.png"></a>
+      <div class="container intro-text">
+        <h1>Have the freedom <br>of choice.</h1>
+        <h3>Opportunity awaits</h3>
+        <div class="apply-button">Apply</div>
+      </div>
+    </div>
+    <div class="why-work text-center">
+      <h1> Why work with us?</h1>
+      <h3> We give you access to jobs, where and when you want to work.<br>Why not?</h3>
+      <ul class="li-around">
+        <li><img src="images/trusted.svg">
+          <p>Trusted</p>
+        </li>
+        <li><img src="images/weekly.svg">
+          <p>Weekly Payment</p>
+        </li>
+        <li><img src="images/helpdesk.svg">
+          <p>Help Desk</p>
+        </li>
+        <li><img src="images/accesswork.svg">
+          <p>Access Work</p>
+        </li>
+        <li><img src="images/flex.svg">
+          <p>Flexibility</p>
+        </li>
+      </ul>
+    </div>
+    <hr>
+    <div class="full-width what-need">
+      <h1 class="text-center">What You Need To Join Us?</h1>
+      <div class="what-need-text">
+        <div style="padding-top:2vh;" class="col s5 left">
+          <h2> Be a Professional with UrbanYou</h2>
+        </div>
+        <div class="col s6 right">
+          <p>UrbanYou works with only the best cleaners and gardeners. To join our network of trusted and reliable service providers, and start growing your business with regular quality leads, you’ll need:</p>
+          <ul>
+            <li>Minimum 2-years’ experience</li>
+            <li>Public liability insurance (we can help you sort this out)</li>
+            <li>Pass our rigorous background checks, including Police Check.</li>
+            <li>Passion for going the extra mile to maintain our 100% satisfaction guarantee.</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class="full-width how-it-work">
+      <h1>How It works?</h1>
+      <div class="list-work"></div>
+    </div>
+  </body>
+</html>
